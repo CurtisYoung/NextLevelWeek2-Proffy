66d47829f387d854155ae5cc62da5302c16a31bd
Curtis Young <curtisyg@gmail.com>
2020-08-07 15:26:39 -0300

    Create index.tsx

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
+import styles from './styles';
+
+function TeacherList() {
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    function handleToggleFiltersVisible(){
+        setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    return (
+        <View style={styles.container}>
+            <PageHeader 
+                title='Professores disponíveis.' 
+                headerRight={(
+                    <BorderlessButton onPress={handleToggleFiltersVisible}>
+                        <Feather name="filter" size={20} color="#FFF"/>
+                    </BorderlessButton>
+                )}
+            >
+                {isFiltersVisible && (<View style={styles.searchForm}>
+                    <Text style={styles.label}>Assunto</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="subject: "
+                        placeholderTextColor="#c1bccc"
+                    />
+
+                    <View style={styles.inputGroup}>
+                        <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Dia</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="week day: "
+                                placeholderTextColor="#c1bccc"
+                            />
+                        </View>
+
+                        <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Hora</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="hour: "
+                                placeholderTextColor="#c1bccc"
+                            />
+                        </View>
+                    </View>
+
+                    <RectButton style={styles.submitButton}>
+                    <Text style={styles.submitButtonText}>Filtrar</Text>
+                    </RectButton>
+                </View>
+                )}
+            </PageHeader>
+
+            <ScrollView
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}
+            >
+                <TeacherItem/>
+                <TeacherItem/>
+                <TeacherItem/>
+                <TeacherItem/>
+                <TeacherItem/>
+                <TeacherItem/>
+            </ScrollView>
+        </View>
+    );
+}
+
+export default TeacherList;
